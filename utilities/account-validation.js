2d2a0f38aa54e79ed72cf211d6877ac21ecb2417
Lisa <lisacjheinhold@gmail.com>
2023-12-01 14:17:24 -0700

    fixed update email validation

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -133,18 +133,21 @@ validate.updateRules = () => {
       .withMessage("Please provide a last name."), // on error this message is sent.
 
       // valid email is required // if email is being changed, it cannot already exist in the database 
-      body("account_email") 
-        .trim() 
-        .isEmail() 
-        .normalizeEmail() // refer to validator.js docs 
-        .withMessage("A valid email is required.") 
-        .custom(async (account_email, { req }) => { 
-          const account_id = req.body.account_id 
-          const account = await accountModel.getAccountById(account_id) // Check if submitted email is same as existing 
-          if (account_email != account.account_email) { // No - Check if email exists in table 
-            const emailExists = await accountModel.checkExistingEmail( account_email ) // Yes - throw error 
-            if (emailExists.count != 0) { throw new Error("Email exists. Please use a different email") } 
-          } 
+      body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required.")
+      .custom(async (account_email, { req }) => {
+        const ExistingEmail = await accountModel.checkExistingEmail(
+          account_email,
+          req.body.account_id
+        );
+        if (ExistingEmail) {
+          throw new Error(
+            "Email exists. Please log in or use different email"
+          );
+        }
       }),
   ]
 }
